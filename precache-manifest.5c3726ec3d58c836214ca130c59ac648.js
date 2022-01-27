883c9360624e8ba870d089cb9d3d19137485c16d
TrungJamin <TrungJamin@users.noreply.github.com>
2022-01-27 11:19:33 +0000

    deploy: d1354649025c014d304eb6e0ca430eebdfc542cb

---
+++ precache-manifest.5c3726ec3d58c836214ca130c59ac648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777f5d5e5e40d0a43e419f23094b1986",
+    "url": "https://trungjamin.github.io/task-management-frontend\n/index.html"
+  },
+  {
+    "revision": "4175a6b257a3f3244106",
+    "url": "https://trungjamin.github.io/task-management-frontend\n/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "548852f69d4362cf1dfe",
+    "url": "https://trungjamin.github.io/task-management-frontend\n/static/js/2.45bd1e58.chunk.js"
+  },
+  {
+    "revision": "4175a6b257a3f3244106",
+    "url": "https://trungjamin.github.io/task-management-frontend\n/static/js/main.271b492b.chunk.js"
+  },
+  {
+    "revision": "4767efa43d45bfea7347",
+    "url": "https://trungjamin.github.io/task-management-frontend\n/static/js/runtime~main.7f44510a.js"
+  }
+]);
